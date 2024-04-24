4e3499af319089330d8f426a94ffa4367a938a51
Shyam Tala <49745830+shyamtala003@users.noreply.github.com>
2024-04-24 12:42:05 +0000

    SETUP : basic rounter setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./components/layouts/MainLayout";
+import SignIn from "./pages/signin/SignIn";
 import Home from "./pages/home/Home";
+import SignUp from "./pages/signup/SignUp";
 
 export default function App() {
   return (
     <MainLayout>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/registration" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </MainLayout>
   );
 }
