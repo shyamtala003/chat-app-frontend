013bb80a28ac5ebf5882c631a7eed1f1c294d53a
Shyam Tala <49745830+shyamtala003@users.noreply.github.com>
2024-04-25 06:44:45 +0000

    Pending changes exported from your codespace

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -2,10 +2,13 @@ import { useForm } from "react-hook-form";
 import MainLayout from "../../components/layouts/MainLayout";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import TextInput from "../../components/formComponents/TextInput";
+import PasswordInput from "../../components/formComponents/PasswordInput";
 
 const schema = yup.object().shape({
   username: yup
     .string()
+    .required("username is required")
     .matches(
       /^[a-zA-Z0-9_]+$/,
       "Username must not contain special characters and spaces"
@@ -21,6 +24,7 @@ const schema = yup.object().shape({
 const SignIn = () => {
   const {
     handleSubmit,
+    register,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
@@ -40,53 +44,21 @@ const SignIn = () => {
           Login to <span className="text-green-300">ChatApp</span>
         </h2>
 
-        <form
-          className="flex flex-col gap-4 mt-6"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <label className="flex items-center gap-2 input input-bordered">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-            </svg>
-            <input
-              type="text"
-              className="grow"
-              placeholder="Username"
-              // {...control("username")}
-            />
-          </label>
-          {errors.username && (
-            <p className="text-red-500">{errors.username.message}</p>
-          )}
-          <label className="flex items-center gap-2 input input-bordered">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input
-              type="password"
-              className="grow"
-              placeholder="Password"
-              // {...control("password")}
-            />
-          </label>
-          {errors.password && (
-            <p className="text-red-500">{errors.password.message}</p>
-          )}
-          <button className="mt-2 text-green-300 btn">Login</button>
+        <form className="flex flex-col mt-6" onSubmit={handleSubmit(onSubmit)}>
+          <TextInput
+            name={"username"}
+            placeholder={"username"}
+            register={register}
+            errors={errors}
+          />
+
+          <PasswordInput
+            name={"password"}
+            placeholder={"password"}
+            register={register}
+            errors={errors}
+          />
+          <button className="mt-4 text-green-300 btn">Login</button>
         </form>
       </div>
     </MainLayout>
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,82 +1,97 @@
+import { useForm } from "react-hook-form";
+import PasswordInput from "../../components/formComponents/PasswordInput";
 import MainLayout from "../../components/layouts/MainLayout";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import TextInput from "../../components/formComponents/TextInput";
+import RadioGroupInput from "../../components/formComponents/RadioGroupInput";
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required("Name is required")
+    .min(4, "Name must be at least 4 characters"),
+  username: yup
+    .string()
+    .required("username is required")
+    .matches(
+      /^[a-zA-Z0-9_]+$/,
+      "Username must not contain special characters and spaces"
+    )
+    .min(6, "Username must be at least 6 characters")
+    .max(24, "Username must not exceed 24 characters"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .max(30, "Password must not exceed 30 characters"),
+  confirmPassword: yup
+    .string()
+    .required("Confirm Password is required")
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+  gender: yup.string().required("Gender is required"),
+});
 
 const SignUp = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  console.log(errors);
+
+  const onSubmit = (data) => console.log(data);
   return (
     <MainLayout>
-      <div className="flex flex-col self-center w-full h-full px-4 py-8 mx-auto bg-gray-600 rounded-r-none md:h-auto md:max-w-96 backdrop-blur-lg bg-clip-padding backdrop-filter bg-opacity-10 md:rounded-2xl">
+      <div className="flex flex-col self-center w-full h-full px-4 py-8 mx-auto bg-gray-600 rounded-r-none md:h-auto md:max-w-lg backdrop-blur-lg bg-clip-padding backdrop-filter bg-opacity-10 md:rounded-2xl">
         <h2 className="text-2xl font-semibold text-center text-white">
           Sign Up to <span className="text-green-300">ChatApp</span>
         </h2>
 
-        <form className="flex flex-col gap-4 mt-6">
-          <label className="flex items-center gap-2 input input-bordered">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-            </svg>
-            <input type="text" className="grow" placeholder="Name" />
-          </label>
-          <label className="flex items-center gap-2 input input-bordered">
-            @
-            <input type="text" className="grow" placeholder="Username" />
-          </label>
-          <label className="flex items-center gap-2 input input-bordered">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input type="password" className="grow" placeholder="Password" />
-          </label>
-          <label className="flex items-center gap-2 input input-bordered">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input
-              type="password"
-              className="grow"
-              placeholder="Confrim password"
-            />
-          </label>
-          <div className="flex gap-4">
-            <label className="inline-flex justify-start gap-3 cursor-pointer label">
-              <input
-                type="radio"
-                name="radio-10"
-                className="border-2 border-white border-opacity-50 radio checked:bg-green-300"
-              />
-              <span className="label-text">Male</span>
-            </label>
+        <form className="flex flex-col mt-6" onSubmit={handleSubmit(onSubmit)}>
+          <TextInput
+            name={"name"}
+            placeholder={"Name"}
+            register={register}
+            errors={errors}
+          />
 
-            <label className="inline-flex justify-start gap-3 cursor-pointer label">
-              <input
-                type="radio"
-                name="radio-10"
-                className="border-2 border-white border-opacity-50 radio checked:bg-green-300"
-              />
-              <span className="label-text">Female</span>
-            </label>
-          </div>
+          <TextInput
+            name={"username"}
+            label="@"
+            placeholder={"Usenrname"}
+            register={register}
+            errors={errors}
+          />
+
+          <PasswordInput
+            name={"password"}
+            placeholder={"Password"}
+            register={register}
+            errors={errors}
+          />
+
+          <PasswordInput
+            name={"confirmPassword"}
+            placeholder={"Confirm password"}
+            register={register}
+            errors={errors}
+          />
+
+          <RadioGroupInput
+            name={"gender"}
+            register={register}
+            valueArray={["male", "female"]}
+            errors={errors}
+          />
 
           <button className="mt-2 text-green-300 btn">Sign Up</button>
         </form>
