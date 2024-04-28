2aba5ec0332e47e8d022617bdecbeef0c4669207
shyamtala003 <shyamatala003@gmail.com>
2024-04-28 23:23:12 +0530

    REFACTOR : separate route from app.js

---
+++ src/router.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/signin/SignIn";
+import Home from "./pages/home/Home";
+import SignUp from "./pages/signup/SignUp";
+import ProtectedRoute from "./components/routeProtector/ProtectedRoute";
+import PublicRoute from "./components/routeProtector/PublicRoute";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <SignIn />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/registration"
+          element={
+            <PublicRoute>
+              <SignUp />
+            </PublicRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
